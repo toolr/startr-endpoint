2950ba6a0300105b486850165492aae053511e54
damondouglas <douglas.damon@gmail.com>
2013-12-25 22:48:57 -0500

    Modified endpoint to provision oauth method.

--- startr_api.py
+++ startr_api.py
@@ -4,14 +4,19 @@ Defined here are the ProtoRPC messages needed to define Schemas for methods
 as well as those methods defined in an API.
 """
 
-
 import endpoints
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
+import json
+
+f = open('client_secret.json','r')
+data = f.read()
+f.close()
+secrets = json.loads(data)
+CLIENT_ID = secrets['web']['client_id']
 
 package = 'Hello'
-
 
 class Greeting(messages.Message):
     """Greeting that stores a message."""
@@ -29,9 +34,13 @@ STORED_GREETINGS = GreetingCollection(items=[
 ])
 
 
-@endpoints.api(name='helloworld', version='v1')
+@endpoints.api(name='helloworld', version='v1',
+               allowed_client_ids=[CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID],
+               audiences=[CLIENT_ID],
+               scopes=[endpoints.EMAIL_SCOPE])
+
 class HelloWorldApi(remote.Service):
-    """Helloworld API v1."""
+    """helloworld API v1."""
 
     MULTIPLY_METHOD_RESOURCE = endpoints.ResourceContainer(
             Greeting,
@@ -43,6 +52,7 @@ class HelloWorldApi(remote.Service):
                       name='greetings.multiply')
     def greetings_multiply(self, request):
         return Greeting(message=request.message * request.times)
+
 
     @endpoints.method(message_types.VoidMessage, GreetingCollection,
                       path='hellogreeting', http_method='GET',
@@ -63,6 +73,13 @@ class HelloWorldApi(remote.Service):
         except (IndexError, TypeError):
             raise endpoints.NotFoundException('Greeting %s not found.' %
                                               (request.id,))
-
+    @endpoints.method(message_types.VoidMessage, Greeting,
+                      path='hellogreeting/authed', http_method='POST',
+                      name='greetings.authed')
+    def greeting_authed(self, request):
+        current_user = endpoints.get_current_user()
+        email = (current_user.email() if current_user is not None
+                 else 'Anonymous')
+        return Greeting(message='hello %s' % (email,))
 
 APPLICATION = endpoints.api_server([HelloWorldApi])
